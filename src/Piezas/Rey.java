dee641240c87bd214329487696b1ef1cae8e37a4
CoderMaik <minimaik001@gmail.com>
2020-06-15 17:00:15 +0200

    "Movimiento Rey"

--- src/Piezas/Rey.java
+++ src/Piezas/Rey.java
@@ -8,6 +8,14 @@ public class Rey extends Pieza {
     public Rey() {}
 
     public boolean mover(Casilla origen, Casilla destino) {
-        throw new RuntimeException("Not implemented yet");
+        if (!inValido(destino) || !origenValido(origen) || !movValido(origen,destino) || (!destino.esLibre() && !destino.esComible(this.getOpColor())))
+            return false;
+        else
+            return true;
+    }
+    private boolean movValido(Casilla origen, Casilla destino){
+        return(!origen.equals(destino) &&
+                Math.abs(destino.getCoorX()-origen.getCoorX())<=1 &&
+                    Math.abs(destino.getCoorY()-origen.getCoorY())<=1);
     }
 }
