ef496f72543610b4275f555b339467dd8835b61f
Rocioooo13 <rocioducid99@gmail.com>
2020-06-15 21:18:34 +0200

    Tablero --> mirar los set

--- src/POO/Tablero.java
+++ src/POO/Tablero.java
@@ -1,14 +1,41 @@
 package POO;
 
 import Piezas.Rey;
+import POO.Casilla;
+import Piezas.Color;
 
 public class Tablero {
     private Casilla[][] mapa;
     //Constructor vacio
-    public Tablero(){
+    public Tablero (){
         mapa = new Casilla[8][8];
     }
     public Tablero(String map){
+        int columna = 0;
+        int fila = 8;
+        int index = 0;
+        while (!map.equals(null)){
+            if (map.charAt(index) == 'V') {
+                mapa[fila][columna].checkPieza(mapa[fila][columna]).setCas() = null;
+            }else if (map.charAt(index) == ','){
+                if (columna == 8){
+                    columna = 1;
+                    fila --;
+                }else{
+                    columna ++;
+                }
+            }else if ((map.charAt(index) == 'B') || (map.charAt(index) == 'N')){
+                if (map.charAt(index) == 'B'){
+                    mapa[fila][columna].checkPieza(mapa[fila][columna]).setColor('BLANCO');
+                }else {
+                    mapa[fila][columna].checkPieza(mapa[fila][columna]).setColor('NEGRO');
+                }
+            }else {
+                mapa[fila][columna].checkPieza(mapa[fila][columna]).setTab();
+                mapa[fila][columna].checkPieza(mapa[fila][columna]).setCas();
+            }
+            index ++;
+        }
         //constructor pasandole un tablero hecho
     }
     public Casilla getCasilla(char x, int y) {
